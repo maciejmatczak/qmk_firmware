bd25d894d26d97e12584aa2437f9ed25e20f0f2b
Maciej Matczak <mmatczak@synopsys.com>
2020-04-26 17:06:04 +0200

    Modify home rows keys with modifiers

--- keyboards/five_by_six/keymaps/default/keymap.c
+++ keyboards/five_by_six/keymaps/default/keymap.c
@@ -28,6 +28,17 @@ enum layers {
 #define RAISE MO(_RAISE)
 #define NUMPAD MO(_NUMPAD)
 
+// Modfied home row keys
+#define M_A LSFT_T(KC_A)
+#define M_S LCTL_T(KC_S)
+#define M_D LGUI_T(KC_D)
+#define M_F LALT_T(KC_F)
+
+#define M_J RALT_T(KC_J)
+#define M_K RGUI_T(KC_K)
+#define M_L RCTL_T(KC_L)
+#define M_SCLN RSFT_T(KC_SCLN)
+
 // Biggest kudos goes for Smittey:
 // https://github.com/Smittey/qmk_firmware/blob/master/keyboards/planck/keymaps/smittey/keymap.c
 // https://www.smittey.co.uk/the-planck-key-theory/
@@ -48,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
     KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-    KC_ESC,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,   KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    KC_ESC,      M_A,     M_S,     M_D,     M_F,    KC_G,        KC_H,    M_J,     M_K,     M_L,  M_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
     KC_LCTL, KC_LGUI, KC_LALT,  NUMPAD,   KC_ENT,  KC_SPC,      LOWER,  RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
   ),
