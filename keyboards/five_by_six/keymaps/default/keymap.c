3fd6b0e9bdeacf9a18a97d5294ea9e65ba4bd2d5
Maciej Matczak <maciek.matczak@gmail.com>
2020-09-07 15:35:51 +0200

    Fix macro name

--- keyboards/five_by_six/keymaps/default/keymap.c
+++ keyboards/five_by_six/keymaps/default/keymap.c
@@ -39,7 +39,7 @@ enum layers {
 #define M_L RCTL_T(KC_L)
 #define M_SCLN RSFT_T(KC_SCLN)
 
-#define M_ESC (LCTL_t(KC_ESC))
+#define M_ESC (LCTL_T(KC_ESC))
 
 // Biggest kudos goes for Smittey:
 // https://github.com/Smittey/qmk_firmware/blob/master/keyboards/planck/keymaps/smittey/keymap.c
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'  `-----------------------------------------'
  */
   [_BASE] = LAYOUT(
-    KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-     M_ESC,      M_A,     M_S,     M_D,     M_F,    KC_G,        KC_H,    M_J,     M_K,     M_L,  M_SCLN, KC_QUOT,
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
-    KC_LCTL, KC_LGUI, KC_LALT,  NUMPAD,   KC_ENT,  KC_SPC,      LOWER,  RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
+    KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
+      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+       M_ESC,     M_A,     M_S,     M_D,     M_F,    KC_G,        KC_H,    M_J,     M_K,     M_L,  M_SCLN, KC_QUOT,
+     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,   KC_M, KC_COMM,  KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
+     KC_LCTL, KC_LGUI, KC_LALT,  NUMPAD,   KC_ENT,  KC_SPC,      LOWER,  RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
 
