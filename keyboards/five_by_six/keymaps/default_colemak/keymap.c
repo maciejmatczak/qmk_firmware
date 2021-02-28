0ca5757e41188e5405f88d06b6433ef02a66dfc7
Maciej Matczak <maciek.matczak@gmail.com>
2021-02-28 15:47:21 +0100

    Dummy fixe math.h overrided symbol

--- keyboards/five_by_six/keymaps/default_colemak/keymap.c
+++ keyboards/five_by_six/keymaps/default_colemak/keymap.c
@@ -35,7 +35,7 @@ enum layers {
 #define M_T LALT_T(KC_T)
 
 #define M_N RALT_T(KC_N)
-#define M_E RGUI_T(KC_E)
+#define M_EE RGUI_T(KC_E)
 #define M_I RCTL_T(KC_I)
 #define M_O RSFT_T(KC_O)
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,   KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
-       M_ESC,     M_A,     M_R,     M_S,     M_T,    KC_G,        KC_M,    M_N,     M_E,     M_I,     M_O, KC_QUOT,
+       M_ESC,     M_A,     M_R,     M_S,     M_T,    KC_G,        KC_M,    M_N,     M_EE,     M_I,     M_O, KC_QUOT,
      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,   KC_H, KC_COMM,  KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
      KC_LCTL, KC_LGUI, KC_LALT,  NUMPAD,   LOWER,  KC_SPC,      KC_ENT,  RAISE, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX
   ),
